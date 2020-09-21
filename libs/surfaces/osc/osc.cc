4c510837e560482e81ff1add4e84db4b0b01450c
Paul Davis <paul@linuxaudiosystems.com>
2020-09-21 14:28:23 -0600

    surfaces: first round of conversion to use timeline types

--- libs/surfaces/osc/osc.cc
+++ libs/surfaces/osc/osc.cc
@@ -42,6 +42,8 @@
 #include <pbd/file_utils.h>
 #include <pbd/failed_constructor.h>
 
+#include "temporal/timeline.h"
+
 #include "ardour/amp.h"
 #include "ardour/session.h"
 #include "ardour/route.h"
@@ -3397,7 +3399,7 @@ OSC::set_marker (const char* types, lo_arg **argv, int argc, lo_message msg)
 				for (Locations::LocationList::const_iterator l = ll.begin(); l != ll.end(); ++l) {
 					if ((*l)->is_mark ()) {
 						if (strcmp (&argv[0]->s, (*l)->name().c_str()) == 0) {
-							session->request_locate ((*l)->start (), MustStop);
+							session->request_locate ((*l)->start_sample (), MustStop);
 							return 0;
 						} else if ((*l)->start () == session->transport_sample()) {
 							cur_mark = (*l);
@@ -3426,7 +3428,7 @@ OSC::set_marker (const char* types, lo_arg **argv, int argc, lo_message msg)
 	// get Locations that are marks
 	for (Locations::LocationList::const_iterator l = ll.begin(); l != ll.end(); ++l) {
 		if ((*l)->is_mark ()) {
-			lm.push_back (LocationMarker((*l)->name(), (*l)->start ()));
+			lm.push_back (LocationMarker((*l)->name(), (*l)->start_sample ()));
 		}
 	}
 	// sort them by position
@@ -4028,11 +4030,11 @@ OSC::touch_detect (const char *path, const char* types, lo_arg **argv, int argc,
 		if (control) {
 			if (touch) {
 				//start touch
-				control->start_touch (control->session().transport_sample());
+				control->start_touch (timepos_t (control->session().transport_sample()));
 				ret = 0;
 			} else {
 				// end touch
-				control->stop_touch (control->session().transport_sample());
+				control->stop_touch (timepos_t (control->session().transport_sample()));
 				ret = 0;
 			}
 			// just in case some crazy surface starts sending control values before touch
@@ -4052,8 +4054,8 @@ OSC::fake_touch (boost::shared_ptr<ARDOUR::AutomationControl> ctrl)
 	if (ctrl) {
 		//start touch
 		if (ctrl->automation_state() == Touch && !ctrl->touching ()) {
-		ctrl->start_touch (ctrl->session().transport_sample());
-		_touch_timeout[ctrl] = 10;
+			ctrl->start_touch (timepos_t (ctrl->session().transport_sample()));
+			_touch_timeout[ctrl] = 10;
 		}
 	}
 
@@ -4332,6 +4334,12 @@ OSC::route_recenable (int ssid, int yn, lo_message msg)
 		}
 	}
 	return float_message_with_id (X_("/strip/recenable"), ssid, 0, sur->feedback[2], get_address (msg));
+}
+
+void
+OSC::loop_location (int start, int end)
+{
+	BasicUI::loop_location (timepos_t (start), timepos_t (end));
 }
 
 int
@@ -6333,7 +6341,7 @@ OSC::periodic (void)
 		if (!(*x).second) {
 			boost::shared_ptr<ARDOUR::AutomationControl> ctrl = (*x).first;
 			// turn touch off
-			ctrl->stop_touch (ctrl->session().transport_sample());
+			ctrl->stop_touch (timepos_t (ctrl->session().transport_sample()));
 			_touch_timeout.erase (x++);
 		} else {
 			x++;
